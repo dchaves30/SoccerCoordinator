fdd59e3281862f4b560bcc10ca41e7da2ca8a5b3
Danilo Chaves <chaves_danilo@emerson.edu>
2016-08-09 00:25:07 -0400

    updated version

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -1,7 +1,35 @@
-//: Playground - noun: a place where people can play
+//Soccer Coordinator Project One
 
-import UIKit
+var counter = 0
 
-var str = "Hello, playground"
+let playerNames: [String:Int] =
+    ["Joe Smith":42,
+     "Jill Tanner":36,
+     "Bill Bon":43,
+     "Eva Gordon":45,
+     "Matt Gill":40,
+     "Kimmy Stein":41,
+    "Sammy Adams":45,
+    "Karl Saygan":42,
+    "Suzane Greenberg":44,
+    "Sal Dali":41,
+    "Joe Kavalier":39,
+    "Ben Finkelstein":44,
+    "Diego Soto":41,
+    "Chloe Alaska":47,
+    "Arnold Willis":43,
+    "Phillip Helm":44,
+    "Les Clay":42,
+    "Herschel Krustofski":45
+]
 
-//Danilo's comment
+
+for height in playerNames.values
+{
+    
+    var bigger = height
+    var smaller = bigger
+    print(smaller)
+    
+}
+ 
